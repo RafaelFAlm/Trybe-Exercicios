178d002fcd42d15f4f64ff3344191f8c59f92eac
RafaelAlmeida <Rafael_Tbm1987@hotmail.com>
2022-03-20 17:45:56 -0300

    5.2 p2

--- fundamentos/Bloco 5 DOM/Exercises5_2/script.js
+++ fundamentos/Bloco 5 DOM/Exercises5_2/script.js
@@ -9,8 +9,30 @@ document.getElementById('pai').firstElementChild;
 // 5 Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta .
 document.getElementById('elementoOndeVoceEsta').firstElementChild;
 // 6 Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta .
-document.getElementById('elementoOndeVoceEsta').firstChild
+document.getElementById('elementoOndeVoceEsta').firstChild;
 // 7 Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta .
-document.getElementById('elementoOndeVoceEsta').nextElementSibling
+document.getElementById('elementoOndeVoceEsta').nextElementSibling;
 // 8 Agora acesse o terceiroFilho a partir de pai .
-document.getElementById('pai').lastElementChild.previousElementSibling
+document.getElementById('pai').lastElementChild.previousElementSibling;
+
+//9 Crie um irmão para elementoOndeVoceEsta .
+let divNova = document.createElement("div");
+let conteudoNovo = document.createTextNode("Elemento irmao foi adicionado");
+divNova.appendChild(conteudoNovo);
+pai.appendChild(divNova);
+
+//10 Crie um filho para elementoOndeVoceEsta .
+let elementoFilho = document.createElement("div");
+let conteudoFilho = document.createTextNode ("Elemento filho de onde vc esta");
+elementoFilho.appendChild(conteudoFilho);
+elementoOndeVoceEsta.appendChild(elementoFilho);
+
+//11 Crie um filho para primeiroFilhoDoFilho .
+let elementoFilhoDoFilho = document.createElement("div");
+let conteudoFilhoDoFilho = document.createTextNode ("Mais um filho chato");
+elementoFilhoDoFilho.appendChild(conteudoFilhoDoFilho);
+primeiroFilhoDoFilho.appendChild(elementoFilhoDoFilho);
+
+//12 A partir desse filho criado, acesse terceiroFilho .
+elementoFilhoDoFilho .classList.add("fDF");
+document.getElementsByClassName('fDF').parentNode.nextElementSibling;
