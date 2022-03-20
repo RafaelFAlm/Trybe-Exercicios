a672a3e5511c4ab90597cb5d73ba1f41cd609714
RafaelAlmeida <Rafael_Tbm1987@hotmail.com>
2022-03-20 13:41:23 -0300

    Exercicios parte 5.1

---
+++ fundamentos/Bloco 5 DOM/DOMtrainingScript.js
@@ -0,0 +1,7 @@
+      /*const paragraph = document.getElementById("paragraph");
+      paragraph.style.color = "blue";*/
+      document.getElementById("page-title").innerText = "Filme preferido";
+      document.getElementById("subtitle").innerText = "Substitulo criativo";
+      document.getElementById("subtitle").style.color = "green";
+      document.getElementsByClassName("paragraph")[0].style.color = "red";
+      document.getElementsByClassName("paragraph")[1].style.color = "blue";
